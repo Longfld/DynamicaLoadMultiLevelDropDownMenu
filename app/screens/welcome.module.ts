70cf696617e2b10ccd53c49a6f2271ba6902f48d
Longfld <Longfld@users.noreply.github.com>
2016-10-07 10:42:39 +1100

    Create welcome.module.ts

---
+++ app/screens/welcome.module.ts
@@ -0,0 +1,16 @@
+import {Component, NgModule }    from '@angular/core';
+import { Routes, RouterModule }  from '@angular/router';
+
+
+@Component({
+    templateUrl: "<h1>welcome</h1> welcome"
+})
+class Welcome { }
+
+const routes: Routes = [{ path: '', component: Welcome}];
+
+@NgModule({
+  imports: [ FormsModule, RouterModule.forChild(routes) ],
+  declarations: [ Welcome]
+})
+export default class WelcomeModule { }
