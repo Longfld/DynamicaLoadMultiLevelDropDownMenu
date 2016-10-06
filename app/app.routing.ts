26fc1d1ecddab35c9dbd08fd792ac598265853b9
Longfld <Longfld@users.noreply.github.com>
2016-10-07 10:44:54 +1100

    Create app.routing.ts

---
+++ app/app.routing.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes= GetScreens();
+
+export const routing = RouterModule.forRoot(routes);
+
+function GetScreens(){
+  var results :Array<Object> = Array<Object>();
+  let screens :Array<string> = ["page1","page2","page3","mypage"]
+
+  results.push({ path: '' ,redirectTo: 'welcome', pathMatch: 'full'});
+  results.push({ path: 'welcome', loadChildren: 'app/pws/welcome.module' })
+  screens.map(screenId => results.push({path: screenId, loadChildren: 'app/pws/' + screenId + '.module' }))
+  return results 
+}
