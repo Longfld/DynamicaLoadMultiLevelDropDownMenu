b572904027163740756571264046cb747e70924b
Longfld <Longfld@users.noreply.github.com>
2016-10-07 10:43:47 +1100

    Create app.component.ts

---
+++ app/app.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewChecked,Component } from '@angular/core';
+import './rxjs-operators';
+
+declare var $: any;
+
+@Component({
+  selector: 'my-app',
+  template: `
+     <my-router-link (onMenusOK)="onMenusOK($event)"></my-router-link>
+    <router-outlet></router-outlet>
+  `
+})
+export class AppComponent implements AfterViewChecked {
+
+     private menuCompleted: boolean = false;
+    private mainMenuCompleted: boolean = false;
+
+     onMenusOK(completed: boolean) { this.menuCompleted = completed;}
+
+    ngAfterViewChecked() {
+       if (this.menuCompleted && !this.mainMenuCompleted) {
+            $('#main-menu').smartmenus();
+        
+            this.mainMenuCompleted = true;
+            console.log("mainMenuCompleted=" + this.mainMenuCompleted)
+        } 
+       console.log("menuCompleted=" + this.menuCompleted)
+    }
+  
+}
