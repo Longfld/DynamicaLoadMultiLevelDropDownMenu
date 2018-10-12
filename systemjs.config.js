d69af501f5b0c044f63f3ce9bfb22dd871ea416f
Longfld <Longfld@users.noreply.github.com>
2018-10-12 16:40:16 +1100

    Update systemjs.config.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -17,17 +17,14 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      // angular testing umd bundles
-      '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
-      '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
-      '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
-      '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
-      '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
-      '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
-      '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-      '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+      '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+      '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
+      '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
+     
       // other libraries
-      'rxjs':                       'npm:rxjs'
+      'rxjs': 'npm:rxjs',
+      'rxjs-compat': 'npm:rxjs-compat',
+      'rxjs-system-bundle': 'npm:rxjs-system-bundle'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -35,9 +32,8 @@
         main: './main.js',
         defaultExtension: 'js'
       },
-      rxjs: {
-        defaultExtension: 'js'
-      }
+      'rxjs': {main: 'index.js', defaultExtension: 'js'},
+      'rxjs/operators': {main: 'index.js', defaultExtension: 'js'}
     }
   });
 })(this);
