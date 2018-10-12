960e73a8cdff6bd35fd6b3c96486b74ac11b907a
Longfld <Longfld@users.noreply.github.com>
2018-10-12 16:45:05 +1100

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
@@ -8,7 +10,7 @@ import { MyRouterLink} from './MyRouterLink';
 
 
 @NgModule({
-  imports:      [ BrowserModule,HttpModule,JsonpModule, routing ],
+  imports:      [ BrowserModule,HttpModule, HttpClientModule,JsonpModule, routing ],
   declarations: [ AppComponent,MyRouterLink ],
   bootstrap:    [ AppComponent ]
 })
